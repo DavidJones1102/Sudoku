c8ca3cda20c9cde484e6e1d27acab72993a92103
Szymon Głomski <szymon.glomski@gmail.com>
2023-06-14 09:56:48 +0200

    solver works

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -1,3 +1,4 @@
+import Board.createZerosArray
 import scalafx.application.JFXApp3
 import scalafx.Includes.*
 import scalafx.geometry.Pos
@@ -7,7 +8,7 @@ import scalafx.scene.control.{Button, Label, Menu, MenuBar, MenuItem}
 import scalafx.scene.layout.{BorderPane, GridPane, HBox, Pane, StackPane, TilePane, VBox}
 
 case class Board(array: Array[Array[Int]]){
-  def isSafe(row:Int, col:Int, num: Int): Boolean = {
+  private def isSafe(row:Int, col:Int, num: Int): Boolean = {
      checkRow(row, num) && checkCol(col,num) && checkBox(row, col, num)
   }
   private def checkRow(row:Int, num: Int): Boolean = {
@@ -17,30 +18,50 @@ case class Board(array: Array[Array[Int]]){
     array.forall(element => element(col) != num)
   }
   private def checkBox(row: Int, col:Int, num:Int): Boolean = {
+    val boxX = col / 3
+    val boxY = row / 3
+    val box = for {
+      yb <- (boxY * 3) until (boxY * 3 + 3) // indices for rows in THIS box
+      xb <- (boxX * 3) until (boxX * 3 + 3) // same for cols
+    } yield array(yb)(xb)
+    !box.contains(num)
+  }
+
+  def checkBoardValidity():Boolean = {
     var flag = true
-    var curr_row = -1
-    var curr_col = -1
-    for(i <- 0 until 3; j<- 0 until 3){
-      curr_row = row/3 + i
-      curr_col = col/3*3 +j
-      if(row != curr_row || col != curr_col) {
-        flag = flag && array(curr_row)(curr_col) != num
+    (0 until 9).foreach( row => (0 until 9).foreach(col => {
+      if array(row)(col) > 0 then {
+        val temp = array(row)(col)
+        array(row)(col) = 0
+        flag &&= isSafe(row,col,temp)
+        array(row)(col) = temp
       }
-    }
+    } ))
     flag
   }
-
-  def solveSudoku(row: Int, col:Int): Unit = {
-    if row >= 9 then println(this.prettyString())
-    else if col >= 9 then solveSudoku(row+1,0)
-    else if array(row)(col) > 0 then solveSudoku(row, col+1)
-    else (1 to 9).filter(value => isSafe(row,col,value)).foreach{ value =>
-      array(row)(col) = value
-      solveSudoku(row,col+1)
-      array(row)(col) = 0
+  def solve(row: Int, col:Int): Boolean = {
+    if (row >= 9) {
+//      println(prettyString())
+      true
     }
-  }
+    else if (col >= 9) solve(row+1, 0)
+    else if (array(row)(col) > 0) solve(row,col+1)
+    else {
+        var flag = false
+        (1 to 9).foreach { value =>
+          if isSafe(row,col, value) then {
+            array(row)(col) = value
+            flag = solve(row,col+1)
+          }
+          if !flag then array(row)(col) = 0
+        }
+        flag
+      }
 
+    }
+  def resetBoard() = {
+    (0 to 8).foreach( row => (0 to 8).foreach( col => array(row)(col) = 0))
+  }
   def prettyString(): String = {
     array.grouped(3).map { bigGroup =>
       bigGroup.map { row =>
@@ -53,10 +74,20 @@ case class Board(array: Array[Array[Int]]){
 }
 
 object Board{
-  def apply(): Board = new Board(createZerosArray())
+  def apply(): Board = new Board(Array(
+    Array(5, 3, 0, 0, 7, 0, 0, 0, 0),
+    Array(6, 0, 0, 1, 9, 5, 0, 0, 0),
+    Array(0, 9, 8, 0, 0, 0, 0, 6, 0),
+    Array(8, 0, 0, 0, 6, 0, 0, 0, 3),
+    Array(4, 0, 0, 8, 0, 3, 0, 0, 1),
+    Array(7, 0, 0, 0, 2, 0, 0, 0, 6),
+    Array(0, 6, 0, 0, 0, 0, 2, 8, 0),
+    Array(0, 0, 0, 4, 1, 9, 0, 0, 5),
+    Array(0, 0, 0, 0, 8, 0, 0, 7, 9),
+  ))
   def apply(array: Array[Array[Int]]): Board = new Board(array)
   private def createZerosArray() = {
-    val array = Array.ofDim[Int](9, 9)
+    var array = Array.ofDim[Int](9, 9)
     for (row <- 0 until 9; col <- 0 until 9) {
       array(row)(col) = 0
     }
--- src/main/scala/SolverGui.scala
+++ src/main/scala/SolverGui.scala
@@ -1,21 +1,25 @@
-import scalafx.Includes.handle
+import scalafx.Includes._
 import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.Scene
-import scalafx.scene.control.{Button, Label, TextField}
+import scalafx.scene.control.Alert.AlertType
+import scalafx.scene.control.{Alert, Button, Label, TextField}
 import scalafx.scene.layout.{GridPane, Region, StackPane, VBox}
 
 object SolverGui {
     private val boardSize = 9
     private val cellSize = 50
-    val board: Board = Board()
+    private val board: Board = Board()
 
     private val gridPane = new GridPane()
     gridPane.padding = Insets(10)
     gridPane.hgap = 1
     gridPane.vgap = 1
-
+    private val textFieldsArray = Array.ofDim[TextField](9,9)
     for (row <- 0 until boardSize; col <- 0 until boardSize) {
       val textField = new TextField()
+      textFieldsArray(row)(col) = textField
+      if board.array(row)(col) > 0 then textField.setText(board.array(row)(col).toString)
+
       textField.textProperty().addListener { (_, oldValue, newValue) =>
           if (!newValue.matches("\\d*")) {
             textField.setText(newValue.replaceAll("[^\\d]", ""))
@@ -31,22 +35,46 @@ object SolverGui {
         }
       textField.prefWidth = cellSize
       textField.prefHeight = cellSize
-      gridPane.add(textField, col, row)
+      gridPane.add(textField, col, row+1)
     }
-    val button = Button("Solve")
+    private val button = Button("Solve")
     button.onAction = handle{
-      board.solveSudoku(0,0)
-//      println(board.prettyString())
+      if(!board.checkBoardValidity()) {
+        new Alert(AlertType.Information, "Invalid board!!!").showAndWait()
+      }
+      else{
+        board.solve(0,0)
+        draw()
+      }
     }
+    private val resetButton = Button("Reset")
+    resetButton.onAction = handle {
+      board.resetBoard()
+//      println(board.prettyString())
+      draw()
+    }
+    private val backButton = Button("Back")
+    backButton.onAction = handle {
+      MainMenu.start()
+    }
+    gridPane.add(button, 4,11)
+    gridPane.add(resetButton, 4,12)
+    gridPane.add(backButton, 0,0)
 
-    gridPane.add(button, 10,4)
-    val scene = new Scene(500,600)
+    val scene = new Scene(500,600){
+      stylesheets += getClass.getResource("solver.css").toExternalForm
+    }
     gridPane.alignment = Pos.Center
 
     scene.root = gridPane
 
-    def solve() = {
-
+    private def draw() = {
+      for(row <- 0 until 9; col <-0 until 9){
+        var text:String = ""
+        if board.array(row)(col) > 0 then text = board.array(row)(col).toString
+        textFieldsArray(row)(col).setText(text)
+      }
     }
+
   }
 
